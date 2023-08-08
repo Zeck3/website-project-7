88fc4b05183708d69caa41066341dcb283603dd9
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-08 23:47:35 +0800

    updated packages and config files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      "background1" : "#202124",
+      "background2" : "#171717",
+      "button-color" : "#303134",
+      "text-white" : "#FFFFFF",
+      "text-grey" : "#7C8085",
+      "red" : "red",
+    },
+  },
+  plugins: [],
+}
+
